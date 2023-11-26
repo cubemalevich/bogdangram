090bbc400c251b48427ccf547c93601360de8246
cubemalevich <cubemalevich1@gmail.com>
2023-11-27 00:39:17 +0300

    Добавил поддержку базы данных для хранения сообщений

--- app.py
+++ app.py
@@ -3,7 +3,24 @@ import re
 from routes import routes
 from mimes import get_mime
 from views import View
+import sqlite3
 
+# Создание подключения к базе данных SQLite
+conn = sqlite3.connect('messages.db')
+
+# Создание курсора для выполнения SQL-запросов
+cursor = conn.cursor()
+
+# Создание таблицы для хранения сообщений, если она не существует
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        message_text TEXT NOT NULL
+    )
+''')
+
+# Закрытие соединения с базой данных
+conn.close()
 
 def load(file_name):
     f = open(file_name, encoding='utf-8')
@@ -18,16 +35,24 @@ def app(environ, start_response):
                   -> body: iterable of strings_
     """
     url = environ['REQUEST_URI']
+    view = None
+    
     for key in routes.keys():
-        print('url', url, 'key', key)
         if re.match(key, url) is not None:
             view = routes[key](url)
-    resp = view.response()
+            break
+    
+    if view is None:
+        view = NotFoundView(url)
+    
+    # Изменение вызова метода response(), передаем только один аргумент
+    resp = view.response(environ)  # Передаем environ в метод response()
     # Возвращаем HTTP-ответ с сгенерированной страницей
-    status = resp.status#'200 OK'
+    status = resp.status    #'200 OK'
     #file_name = route(environ['REQUEST_URI'])
     response_headers = resp.headers
     start_response(status, response_headers)
     return [bytes(resp.data, "utf-8")]
+
    
  
